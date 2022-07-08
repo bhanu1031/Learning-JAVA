3344e17f688ed057794bf91ff71b00bfc0438d2a
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2022-07-08 19:50:55 +0530

    Add files via upload

---
+++ Day_4/for_loop.java
@@ -0,0 +1,27 @@
+package Day_4;
+
+public class for_loop {
+    public static void main(String args[]) {
+        for (int counter=0; counter<3; counter=counter+1){
+            System.out.println("Hello World");
+        }
+        //To print numbers from 1-10
+        for (int count=1; count<11; count=count+1){
+            System.out.println(count);
+        }
+        //To print sum of n numbers
+        int n=6;
+        int sum=0;
+        for(int i=1; i<=n; i=i+1){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+        //To print table of 2
+        int m=3;
+        for (int x=m; x<=m*10; x=x+m){
+            System.out.println(x);
+        }
+
+    }
+}
---
+++ Day_4/while_loop.java
@@ -0,0 +1,12 @@
+package Day_4;
+
+public class while_loop {
+    public static void main(String args[]) {
+        //To print numbers from 0-10 using while loop
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
