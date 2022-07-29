69e4b39afd6f940e25020bd42c1ff46349684b8a
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2022-07-29 22:02:40 +0530

    Add files via upload

---
+++ Day_10/arrays_case1.java
@@ -0,0 +1,11 @@
+package Day_10;
+
+public class arrays_case1 {
+    public static void main(String args[]) {
+        int[] marks= new int[3];
+        marks[0]= 98; //physics
+        marks[1]= 97; //maths
+        marks[2]= 96; //chem
+        System.out.println(marks[1]);
+    }
+}
