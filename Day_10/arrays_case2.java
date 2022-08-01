824074a3082f1b6b343014e214e4b8f88fbba150
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2022-08-01 20:22:24 +0530

    Add files via upload

---
+++ Day_10/arrays_case2.java
@@ -0,0 +1,13 @@
+package Day_10;
+import java.util.*;
+public class arrays_case2 {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter: ");
+        int size= sc.nextInt();
+        int[] marks= new int[size];
+        for(int i=0; i<size; i++){
+            System.out.println(size);
+        }
+    }
+}
