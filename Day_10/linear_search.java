fae16c3cd9cb6b3b881bbd586d982ecf7e0856a1
Bhanu Prakash <77429972+bhanu1031@users.noreply.github.com>
2022-08-02 19:39:55 +0530

    Add files via upload

---
+++ Day_10/linear_search.java
@@ -0,0 +1,28 @@
+package Day_10;
+import java.util.*;
+public class linear_search {
+    /*Problem: Take an array as input from the user. Search for a given number x and
+        print the index at which it occurs.*/
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int  size= sc.nextInt();
+        int[] marks= new int[size];
+        
+        //Input for array
+        System.out.println("Array Input:");
+        for(int i=0; i<marks.length; i++){
+            marks[i]= sc.nextInt();
+        }
+
+        int x= sc.nextInt();
+
+        //Output
+        System.out.println("Output");
+        for(int i=0; i<marks.length; i++){
+            if(marks[i]==x){
+                System.out.println("x found at: "+ i);
+            }
+            System.out.println(marks[i]);
+        }
+    }
+}
